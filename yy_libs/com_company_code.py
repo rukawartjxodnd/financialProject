03537d6a655b9ff2008cbf1e2e712928963d4171
taswoong Seo <rtjxodnd@gmail.com>
2018-11-21 16:12:40 +0900

    20181121

--- yy_libs/com_company_code.py
+++ yy_libs/com_company_code.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import psycopg2 as pg2
 
 def getCompanyCodes(kind = ''):
     url = "http://bigdata-trader.com/itemcodehelp.jsp"
@@ -22,4 +23,58 @@ def getCompanyCodes(kind = ''):
 
         if kind in sosok and name[-1] not in ['우', 'B']:
             resultCodes.append(returnValue)
+    return resultCodes
+
+def CodeDataToDB (codeLists):
+
+    # DB Connect
+    connection = pg2.connect(host='localhost', dbname='postgres', user='postgres', password='1111')
+    cursor = connection.cursor()
+
+    # table clear
+    cursor.execute("delete from company_code")
+    cursor.execute('commit')
+
+    # codeLists별 DB insert
+    sql = "insert into company_code(code, name, kind, last_update_day)values (%s, %s, %s, current_date);"
+    for code in codeLists:
+        try:
+            cursor.execute(sql, (code['code'], code['code_name'][7:], code['sosok'],))
+        except:
+            print("error: "+ code['code'], code['code_name'][7:], code['sosok'])
+        finally:
+            cursor.execute('commit')
+
+    # DB종료, DB연결 해제
+    connection.commit
+    cursor.close
+    connection.close
+
+
+def dataToCode (kind = ''):
+
+    # DB Connect
+    connection = pg2.connect(host='localhost', dbname='postgres', user='postgres', password='1111')
+    cursor = connection.cursor()
+
+    # 리스트 조회
+    sql = "select code, name, kind from company_code where kind =  %s or '' = %s"
+    cursor.execute(sql, (kind, kind,))
+    result = cursor.fetchall()
+
+    # DB종료, DB연결 해제
+    cursor.close
+    connection.close
+
+    # return List
+    resultCodes = []
+
+    for data in result:
+        code = data[0]
+        name = data[1]
+        sosok = data[2]
+        code_name = code+" "+name
+        returnValue = {"code": code, "code_name": code_name, "sosok": sosok}
+        resultCodes.append(returnValue)
+
     return resultCodes
